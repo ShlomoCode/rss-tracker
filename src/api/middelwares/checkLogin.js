b807d3b183ef452b6b5bf371af95fd908f0d1705
ShlomoCode <shlomomail16@gmail.com>
2022-03-07 23:21:14 +0200

    Update checkLogin.js

--- src/api/middelwares/checkLogin.js
+++ src/api/middelwares/checkLogin.js
@@ -4,8 +4,7 @@ const config = require('../../../config.json')
 const checkLogin = function (req, res, next) {
     try {
         const token = req.headers.authorization.replace("Bearer ", "")
-        const infoLogin = jwt.verify(token, config.JWT_KEY, { complete: true })
-        const { email, id } = infoLogin.payload
+        jwt.verify(token, config.JWT_KEY)
         next()
     } catch (error) {
         return res.status(401).json({
