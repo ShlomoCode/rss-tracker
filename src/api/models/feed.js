9310ae7ad127e626f4e47bde66f73859be433308
ShlomoCode <shlomomail16@gmail.com>
2022-03-07 22:01:56 +0200

    fix regex

--- src/api/models/feed.js
+++ src/api/models/feed.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const feedSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     title: { type: String, required: true },
-    url: { type: String, match: /^https?:\/\/\w+\.\w{1,6}/ },
+    url: { type: String, match: /^https?:\/\/[\w-]+\.\w{2,6}/ },
     LastCheckedOn: { type: Date, default: Date.now },
     Subscribers: { type: Array }
 });
