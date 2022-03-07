5f9bf952ad628f5f622866c6bc203d086731791e
ShlomoCode <shlomomail16@gmail.com>
2022-03-07 20:11:54 +0200

    Update app.js

--- app.js
+++ app.js
@@ -5,13 +5,61 @@ const morgan = require("morgan");
 const config = require('./config.json')
 
 // התחברות לדאטה בייס
-mongoose.connect(config.MONGO_URI,{
+mongoose.connect(config.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
 
-mongoose.connection.on('connected', ()=>{
+mongoose.connection.on('connected', () => {
     console.log("mongoDB connected!");
 })
 
 app.use(morgan("dev"))
+
+app.use(express.json())
+app.use(express.urlencoded({
+    extended: false
+}))
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Haeders", "Origin, X-Requested-Wite, Content-Type, Accept, Authorization");
+    if (req.method === "OPTIONS") {
+        res.header("Access-Control-Allow-Methods", "PUT", "POST", "PETCH", "DELETE", "GET");
+        return res.status(200).json({
+            message: "GET OPTIONS"
+        });
+    }
+    next();
+})
+
+// Routes
+const usersRoutes = require('./src/api/routes/users')
+const feedsRoutes = require('./src/api/routes/feeds')
+
+app.use("/api/users", usersRoutes)
+app.use("/api/feeds", feedsRoutes)
+
+
+app.use((req, res, next) => {
+    const error = new Error("Not Found")
+    error.status = 404
+    next(error)
+})
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500)
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+})
+
+// server
+const http = require("http")
+const port = 8080
+
+const server = http.createServer(app)
+
+server.listen(port)
