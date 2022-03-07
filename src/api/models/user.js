2f12c28c3edb03ce2fdeb704b90127a8ed25743b
ShlomoCode <shlomomail16@gmail.com>
2022-03-07 20:08:42 +0200

    Create user schema

---
+++ src/api/models/user.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    email: {
+        type: String,
+        required: true,
+        match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+    },
+    password: { type: String, required: true },
+    name: { type: String, match: /[0-9-\ א-תA-z]{3,15}/, maxLength: 15, required: true },
+    verifi: { type: Boolean, default: false }
+});
+
+module.exports = mongoose.model('User', userSchema);
