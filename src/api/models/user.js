24ca410efeb188417e7701aa74f3c2aeb26dc95c
ShlomoCode <shlomomail16@gmail.com>
2022-03-08 11:18:08 +0200

    Update userSchema

--- src/api/models/user.js
+++ src/api/models/user.js
@@ -9,7 +9,8 @@ const userSchema = mongoose.Schema({
     },
     password: { type: String, required: true },
     name: { type: String, match: /[0-9-\ א-תA-z]{3,15}/, maxLength: 15, required: true },
-    verifi: { type: Boolean, default: false }
+    verifiEmail: { type: Boolean, default: false },
+    registrationDate: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model('User', userSchema);
