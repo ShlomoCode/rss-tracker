2a5a145515ade45bfe4095b713470744c2541fb5
ShlomoCode <shlomomail16@gmail.com>
2022-03-14 16:47:09 +0200

    Update OPTIONS (app.js)

--- app.js
+++ app.js
@@ -26,9 +26,9 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-Wite, Content-Type, Accept, Authorization');
     if (req.method === 'OPTIONS') {
-        res.header('Access-Control-Allow-Methods', 'PUT', 'POST', 'PATCH', 'DELETE', 'GET');
+        res.header('Access-Control-Allow-Methods', 'POST', 'PATCH', 'DELETE', 'GET');
         return res.status(200).json({
-            message: 'GET OPTIONS'
+            message: 'OPTIONS'
         });
     }
     next();
