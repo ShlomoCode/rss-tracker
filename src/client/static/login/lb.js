026f041aa7498f29cf5c850b31df94f0b524c4eb
ShlomoCode <shlomomail16@gmail.com>
2022-03-14 10:28:25 +0200

    שיפור ממשק ההתחברות וההרשמה - הודעות קופצות עם awesome-notifications

--- src/client/static/login/lb.js
+++ src/client/static/login/lb.js
@@ -11,7 +11,13 @@ async function login (input) {
             password: password
         });
     } catch (error) {
-        return console.log(error);
+        if (error.response.status === 401) {
+            new AWN({ position: 'bottom-left' }).alert(`שם המשתמש או הסיסמה שגויים
+            <br>...נסה שוב`);
+        } else {
+            new AWN().alert(`${error.response.status}: ${error.response.data.message}`);
+        }
+        return console.log(error.response);
     }
     const { token } = response.data;
     Cookies.set('token', token, { expires: 7 });
@@ -21,16 +27,38 @@ async function login (input) {
 async function signup (input) {
     const { name, email, password } = input;
 
-    let response;
-    try {
-        response = await axios.post('/users/signup', { email, password, name })
-    } catch (error) {
-        return console.log(error);
+    if (!/[0-9-\ א-תA-z]{3,15}/.test(name)) {
+        return new AWN({ position: 'bottom-left' }).alert('!שם חסר/לא תקין');
     }
-    console.log(response.data);
-    showLoginForm()
-    $('#login-email').val(email);
-    $('#login-password').val(password);
+
+    const notifierSignup = new AWN({
+        position: 'bottom-left',
+        // labels: { async: 'מבצע הרשמה' },
+        messages: {
+            // async: '...אנא המתן',
+            'async-block': '...מבצע הרשמה'
+        }
+    });
+    notifierSignup.asyncBlock(axios.post('/users/signup', { email, password, name }),
+        (resp) => {
+            onResolveRegister();
+            notifierSignup.success(`${resp.data.message}.<br> Please login...`);
+        },
+        (err) => {
+            if (err.response.data.message === 'Email exists') {
+                notifierSignup.alert('המייל הזה כבר רשום<br>...נסה להתחבר', { position: 'bottom-left' });
+                onResolveRegister();
+            } else {
+                notifierSignup.alert(`${err.response.status}: ${err.response.data.message}`, { position: 'bottom-left' });
+            }
+            console.log(err.response);
+        });
+
+    function onResolveRegister () {
+        showLoginForm();
+        $('#login-email').val(email);
+        $('#login-password').val(password);
+    }
 }
 
 export {
