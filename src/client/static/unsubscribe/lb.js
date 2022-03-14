b6dcb4f865a65118ca0ffcfc8d898a68e2b30cdc
ShlomoCode <shlomomail16@gmail.com>
2022-03-14 16:46:41 +0200

    Delete lb.js

--- src/client/static/unsubscribe/lb.js
+++
@@ -1,2 +0,0 @@
-const url = location.href.match(/(https?:\/\/.+(\..+)?)\/unsubscribe\?userID=.+&email=(?<email>[a-z]+@[a-z]+\.[a-z]{2,6})/);
-document.getElementById('email').innerHTML = url.groups.email;
