d95adc9c491903375f0ffe41d614d303c3c81bdb
ShlomoCode <shlomomail16@gmail.com>
2022-04-13 19:25:32 +0300

    add log in connecting to mongo starting (minor)

--- app.js
+++ app.js
@@ -66,6 +66,7 @@ function sleep (ms) {
 };
 (async () => {
     // התחברות לדאטה בייס
+    console.log('connecting to mongo...');
     await mongoose.connect(process.env.MONGO_URI, {
         useNewUrlParser: true,
         useUnifiedTopology: true
