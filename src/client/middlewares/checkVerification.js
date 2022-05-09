5afd6179fdae8c553ae0a3f8ded7f81a83465d08
ShlomoCode <shlomomail16@gmail.com>
2022-05-09 19:29:17 +0300

    הוספת דף אישור אימות דוא"ל והגשתו אם המייל כבר מאומת

--- src/client/middlewares/checkVerification.js
+++ src/client/middlewares/checkVerification.js
@@ -3,8 +3,8 @@ const path = require('path');
 const checkVerification = (req, res, next) => {
     const { user } = res.locals;
     const regexVerifyPage = /^\/verify\/(\?verifyCode=[0-9]{5,6})?$/;
-        return res.redirect('/');
     if (user.verified && regexVerifyPage.test(req.originalUrl)) {
+        return res.sendFile(path.join(__dirname, '../views/', 'verified-ok.html'));
     }
     if (!user.verified && !regexVerifyPage.test(req.originalUrl)) {
         return res.redirect('/verify/');
