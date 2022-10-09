091b3d2167ee185e12cc0307219f4e6c433db245
שלמה <78599753+ShlomoCode@users.noreply.github.com>
2022-10-09 03:18:19 +0300

    הוספת אינדקס תפוגה לסשנים (30 יום)

--- src/api/models/session.js
+++ src/api/models/session.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 
 const sessionSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    userId: { type: mongoose.Schema.Types.ObjectId, required: true }
+    userId: { type: mongoose.Schema.Types.ObjectId, required: true },
+    createdAt: { type: Date, default: Date.now },
+    expires: { type: Date, default: Date.now, expires: 60 * 60 * 24 * 30 } // 30 days
 });
 
 module.exports = mongoose.model('Session', sessionSchema);
